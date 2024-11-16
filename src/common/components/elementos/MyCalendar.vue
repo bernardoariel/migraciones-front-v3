c6d7e84b37f5d8da406446660d6504e7737dc2da
Ezequiel Flores <jeftikas@gmail.com>
2024-11-16 03:53:43 -0300

    changes in the calendar

--- src/common/components/elementos/MyCalendar.vue
+++ src/common/components/elementos/MyCalendar.vue
@@ -1,67 +1,65 @@
 <template>
-  <div class="relative">
-    <!-- Label para el input -->
-    <!-- <label for="calendar" class="input input-bordered flex items-center gap-2">
-        <span>{{ label }}</span>
-      -->
-
-    <!-- Input del calendario -->
-    <input
-      type="text"
-      id="calendar"
-      class="input input-bordered w-full max-w-xs mt-2"
-      :placeholder="props.placeholder"
-      v-model="selectedDate"
-    />
-    <!-- </label> -->
-  </div>
-</template>
-
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import Litepicker from 'litepicker';
-import type { ILPConfiguration } from 'litepicker/dist/types/interfaces';
-
-interface Props {
-  label: string;
-  placeholder?: string;
-}
-
-const props = defineProps<Props>();
-interface CustomLitepickerConfig extends ILPConfiguration {
-  onSelect?: (date: Date | null) => void;
-}
-
-const emit = defineEmits(['update:modelValue']);
-const selectedDate = ref('');
-
-onMounted(() => {
-  const config: CustomLitepickerConfig = {
-    element: document.getElementById('calendar') as HTMLElement,
-    singleMode: true,
-    format: 'DD-MM-YYYY',
-    dropdowns: {
-      minYear: 1900,
-      maxYear: new Date().getFullYear(),
-      months: true,
-      years: true,
-    },
-    onSelect: (date: Date | null) => {
-      if (date) {
-        selectedDate.value = date.toLocaleDateString('es-ES', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-        });
-        emit('update:modelValue', selectedDate.value);
-      }
-    },
-  };
-
-  new Litepicker(config);
-});
-</script>
-
-<style scoped>
-/* Personalización adicional si es necesario */
-</style>
+    <div class="relative">
+      <!-- Input del calendario -->
+      <input
+        type="text"
+        id="calendar"
+        class="input input-bordered w-full max-w-xs mt-2"
+        :placeholder="props.placeholder"
+        v-model="selectedDate"
+      />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import Litepicker from 'litepicker';
+  import type { ILPConfiguration } from 'litepicker/dist/types/interfaces';
+  
+  interface Props {
+    label: string;
+    placeholder?: string;  
+  }
+  
+  const props = defineProps<Props>();
+  interface CustomLitepickerConfig extends ILPConfiguration {
+    onSelect?: (date: Date | null) => void;
+  }
+  
+  const emit = defineEmits(['update:modelValue']);
+  const selectedDate = ref<string | null>(null);  
+  
+  onMounted(() => {
+    const config: CustomLitepickerConfig = {
+      element: document.getElementById('calendar') as HTMLElement,
+      singleMode: true,
+      format: 'DD-MM-YYYY',
+      dropdowns: {
+        minYear: 1900,
+        maxYear: new Date().getFullYear(),
+        months: true,
+        years: true,
+      },
+      onSelect: (date: Date | null) => {
+        if (date) {         
+          selectedDate.value = date.toLocaleDateString('es-ES', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+          });
+          emit('update:modelValue', selectedDate.value); 
+        } else {
+          selectedDate.value = null;
+          emit('update:modelValue', selectedDate.value); 
+        }
+      },
+    };
+  
+    new Litepicker(config);
+  });
+  </script>
+  
+  <style scoped>
+  /* Personalización adicional si es necesario */
+  </style>
+  
