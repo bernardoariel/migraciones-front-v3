76de3e17a12eb09bc40948e6e71a5005e82757ca
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-11-26 18:22:11 -0300

    Agregando CardValidation

--- src/views/NuevaSolicitud.vue
+++ src/views/NuevaSolicitud.vue
@@ -81,85 +81,8 @@
       ></component>
     </div>
 
-    <!-- Nueva Card -->
     <div class="flex-[1] max-h-[85vh] overflow-auto">
-      <div
-        class="w-full mb-2 select-none border-l-4 border-red-400 bg-red-100 p-4 font-medium hover:border-red-500"
-      >
-        <div class="flex justify-between items-center">
-          <div class="avatar-group -space-x-6 rtl:space-x-reverse">
-            <div class="avatar placeholder">
-              <div class="bg-neutral text-neutral-content w-8">
-                <span>?</span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <button class="btn glass btn-xs" @click="personStore.resetState('menores')">
-              + Menores
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        class="w-full mb-2 select-none border-l-4 border-yellow-400 bg-yellow-100 p-4 font-medium hover:border-yellow-500"
-      >
-        <div class="flex justify-between items-center">
-          <div class="avatar-group -space-x-6 rtl:space-x-reverse">
-            <div class="avatar">
-              <div class="w-8">
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-
-            <div class="avatar placeholder">
-              <div class="bg-neutral text-neutral-content w-8">
-                <span>2</span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <button class="btn glass btn-xs" @click="personStore.resetState('autorizantes')">
-              + Autorizante
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        class="w-full mb-2 select-none border-l-4 border-blue-400 bg-blue-100 p-4 font-medium hover:border-blue-500"
-      >
-        <div class="flex justify-between items-center">
-          <div class="avatar-group -space-x-6 rtl:space-x-reverse">
-            <div class="avatar">
-              <div class="w-8">
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <div class="avatar">
-              <div class="w-8">
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-
-            <div class="avatar placeholder">
-              <div class="bg-neutral text-neutral-content w-8">
-                <span>2</span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <button class="btn glass btn-xs" @click="personStore.resetState('acompaneantes')">
-              + Acompañante
-            </button>
-          </div>
-        </div>
-      </div>
+      <CardValidation />
     </div>
   </div>
 </template>
@@ -173,6 +96,7 @@ import FormMenor from '@/modules/migraciones/menores/components/FormMenor.vue';
 import FormAutorizante from '@/modules/migraciones/autorizantes/components/FormAutorizante.vue';
 import FormAcompaneante from '@/modules/migraciones/acompaneantes/components/FormAcompaneante.vue';
 import { useOrdenStore } from '@/common/store/ordenStore';
+import CardValidation from '@/common/components/CardValidation.vue';
 
 type ActiveCategory = 'menores' | 'autorizantes' | 'acompaneantes';
 const childRef = ref();
