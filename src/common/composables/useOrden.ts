7bf7a50a6075ed7fdb00928185611ca9e3b7f09c
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-12-01 21:33:28 -0300

    Agregando

---
+++ src/common/composables/useOrden.ts
@@ -0,0 +1,79 @@
+import { ref, watch, unref, computed } from 'vue';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/vue-query';
+
+import { apiMigrationsData } from '@/api/apiMigrationsData';
+import { OrdenSolicitud } from '../interfaces/orders.interface';
+
+export const getOrdenById = async (id: number) => {
+  try {
+    const response = await apiMigrationsData.get(`/v2/personaById/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener el acompañante con ID ${id}:`, error);
+    throw error;
+  }
+};
+const useOrden = (idOrden: Ref<number | null> | ComputedRef<number | null>) => {
+  const orden = ref<OrdenSolicitud | null>(null);
+
+  const queryClient = useQueryClient();
+
+  const { data, refetch } = useQuery({
+    queryKey: computed(() => ['orden', unref(idOrden)]),
+    queryFn: async () => {
+      const id = unref(idOrden);
+      if (!id || isNaN(id)) {
+        throw new Error('ID no válido o no numérico');
+      }
+      return getOrdenById(id);
+    },
+    enabled: computed(() => {
+      const id = unref(idOrden);
+      return !!id && !isNaN(id);
+    }),
+    staleTime: 1000 * 30,
+  });
+
+  watch(
+    data,
+    (newData) => {
+      person.value = newData || null;
+    },
+    { immediate: true },
+  );
+  const createOrden = async (payload) => {
+    const response = await apiMigrationsData.post(`/v2/persona/new`, payload);
+    queryClient.invalidateQueries({ queryKey: ['ordenes'] });
+    return response.data;
+  };
+
+  const updateOrden = async (payload) => {
+    const id = unref(idOrden);
+    if (!id) throw new Error('ID inválido para actualización');
+    return apiMigrationsData.put(`/v2/agregarorden/${id}`, payload);
+  };
+  const mutation = useMutation({
+    mutationFn: async (payload) => {
+      const id = unref(idOrden);
+      if (id === null || id === 'new') {
+        return createPerson(payload);
+      } else {
+        return updatePerson(payload);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['ordenes'] });
+      refetch();
+    },
+  });
+  return {
+    orden,
+    refetch,
+    mutatePerson: mutation.mutateAsync,
+    isLoading: mutation.isLoading,
+    createOrden,
+    updateOrden,
+  };
+};
+
+export default usePerson;
---
+++ src/common/composables/useOrdenes.ts
@@ -0,0 +1,41 @@
+import { watch } from 'vue';
+import { useQuery } from '@tanstack/vue-query';
+import { storeToRefs } from 'pinia';
+
+import { useOrdersStore } from '../store/ordersStore';
+import type { OrdenSolicitud } from '../interfaces/orders.interface';
+import { OrdenSolicitud } from '../interfaces/orders.interface';
+export const getAllOrdenes = async () => {
+  try {
+    const response = await apiMigrationsData.get('v2/persona/ordenestodos');
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+    throw error;
+  }
+};
+const useOrdenes = () => {
+  const store = useOrdersStore();
+  const { currentPage, orders } = storeToRefs(store);
+
+  const itemsPerPage = 10; // Número de elementos por página
+
+  const { isLoading, data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: () => getAllOrdenes(),
+    staleTime: 1000 * 30,
+  });
+  watch(data, (newOrders: OrdenSolicitud) => {
+    if (newOrders) {
+      store.setOrders(newOrders);
+    }
+  });
+  return {
+    isLoading,
+    orders,
+    currentPage,
+    itemsPerPage,
+  };
+};
+
+export default useOrdenes;
